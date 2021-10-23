99f63979aa1f9aedf0d1b42c4b3434e79cadf1af
Sajal Gupta <guptasajal411@gmail.com>
2021-10-23 20:28:15 +0530

    feat: :rocket: Add courier client with authorization

--- controllers/storageController.js
+++ controllers/storageController.js
@@ -1,10 +1,11 @@
+const CourierClient = require("@trycourier/courier").default;
 const storageModel = require("../models/storageModel.js");
 const Kisaan = require("../models/kisaanModel");
 const path = require("path");
 
-exports.getStatus = function(req, res){
-    storageModel.find({}, function(err, foundStorages){
-        if (err){
+exports.getStatus = function (req, res) {
+    storageModel.find({}, function (err, foundStorages) {
+        if (err) {
             res.send(err);
         } else {
             res.render("status.ejs", { foundStorages: foundStorages });
@@ -12,20 +13,33 @@ exports.getStatus = function(req, res){
     });
 }
 
-exports.postStatus = function(req, res){
-    storageModel.findOne({ storageName: req.body.storageName }, async function(err, foundStorage){
-        if (err){
+exports.postStatus = function (req, res) {
+    storageModel.findOne({ storageName: req.body.storageName }, async function (err, foundStorage) {
+        if (err) {
             res.send(err);
         } else {
             foundStorage.availableStorage = req.body.availableStorage;
             await foundStorage.save();
+            const courier = CourierClient({ authorizationToken: process.env.AUTH_TOKEN });
+            const { messageId } = await courier.send({
+                brand: "1M05TH69ZW4HQBN1BZDN6HWX9HPA",
+                eventId: "KMHX6KM7WAMFX5Q9NMAHE9WQW0RR",
+                recipientId: "f479e483-0174-4d5a-94ea-71307001b70b",
+                profile: {
+                    email: "priyankaafssulur@gmail.com",
+                },
+                data: {
+                },
+                override: {
+                },
+            });
             res.redirect("/status");
-            // Kisaan.find({}, function(err, foundKisaan){
+            // Kisaan.find({}, function (err, foundKisaan) {
             //     if (err) {
             //         res.send(err);
             //     } else {
-            //         foundKisaan.yeild.forEach(function(crop){
-            //             if (crop.quantity < req.body.availableStorage){
+            //         foundKisaan.yeild.forEach(async function (crop) {
+            //             if (crop.quantity < req.body.availableStorage) {
             //                 // send notification to kisaan
             //             }
             //         });
@@ -33,9 +47,8 @@ exports.postStatus = function(req, res){
             // });
         }
     });
-
 }
 
-exports.getHome = function(req, res){
+exports.getHome = function (req, res) {
     res.render(path.join(__dirname, "../views/index"));
 }
