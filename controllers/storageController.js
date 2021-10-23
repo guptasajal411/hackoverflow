347ca5bb3d8cda6966b2176392c08c9eeb5c9437
Sajal Gupta <guptasajal411@gmail.com>
2021-10-23 22:44:39 +0530

    feat: :rocket: Courier integration successful

--- controllers/storageController.js
+++ controllers/storageController.js
@@ -1,7 +1,8 @@
 const storageModel = require("../models/storageModel.js");
 const Kisaan = require("../models/kisaanModel");
 const path = require("path");
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+const { CourierClient } = require("@trycourier/courier");
 
 exports.getStatus = function (req, res) {
     storageModel.find({}, function (err, foundStorages) {
@@ -25,30 +26,23 @@ exports.postStatus = function (req, res) {
                 if (err) {
                     res.send(err);
                 } else {
-                    if (foundKisaan){
-                        foundKisaan.forEach(function(kisaan){
-                            kisaan.yeild.forEach(function(crop){
-                                if (crop.cropName == req.body.storageName){
-                                    if(crop.quantity < req.body.availableStorage){
-                                        var transporter = nodemailer.createTransport({
-                                            service: 'gmail',
-                                            auth: {
-                                                user: process.env.email,
-                                                pass: process.env.emailPassword
-                                            }
-                                        });
-                                        var mailOptions = {
-                                            from: process.env.email,
-                                            to: kisaan.email,
-                                            subject: "There's space for your crops!",
-                                            html: `Hi, ` + kisaan.name + `! Your ` + crop.cropName + ` can be now stored at the warehouse!`
-                                        };
-                                        transporter.sendMail(mailOptions, function (error, info) {
-                                            if (error) {
-                                                res.send(error);
-                                            } else {
-                                                res.redirect("/status");
-                                            }
+                    if (foundKisaan) {
+                        foundKisaan.forEach(function (kisaan) {
+                            kisaan.yeild.forEach(async function (crop) {
+                                if (crop.cropName == req.body.storageName) {
+                                    if (crop.quantity < req.body.availableStorage) {
+                                        const courier = CourierClient({ authorizationToken: process.env.AUTH_TOKEN });
+                                        const { messageId } = await courier.send({
+                                            brand: "1M05TH69ZW4HQBN1BZDN6HWX9HPA",
+                                            eventId: "67F4VDDBR3MJPYKYMJ1B2ANFN5A6",
+                                            recipientId: "95298f93-0ee3-4650-9c10-e57482a2c1c3",
+                                            profile: {
+                                                email: process.env.email,
+                                            },
+                                            data: {
+                                            },
+                                            override: {
+                                            },
                                         });
                                     }
                                 }
